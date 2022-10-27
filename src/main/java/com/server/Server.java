6cb1345dd15d83febe6845ee3d74801aabbff82d
PeterNTA <Nta02052001@gmail.com>
2022-10-27 16:49:39 +0700

    Tạo kết nối socket

--- src/main/java/com/server/Server.java
+++ src/main/java/com/server/Server.java
@@ -9,9 +9,41 @@ package com.server;
  *
  * @author peter
  */
+import java.io.*;
+import java.net.Socket;
+import java.net.ServerSocket;
 public class Server {
+    public static void main(String argv[]) throws Exception
+    {
+        String sentence_from_client;
+        String sentence_to_client;
+        
+        //Tạo socket server, chờ tại cổng '6543'
+        ServerSocket welcomeSocket = new ServerSocket(6543);
+        
+        while(true) {
+            //chờ yêu cầu từ client
+            Socket connectionSocket = welcomeSocket.accept();
+            
+            //Tạo input stream, nối tới Socket
+            BufferedReader inFromClient =
+                new BufferedReader(new
+                    InputStreamReader(connectionSocket.getInputStream())); 
+            
+            //Tạo outputStream, nối tới socket
+            DataOutputStream outToClient =
+                new DataOutputStream(connectionSocket.getOutputStream());
+            
+            //Đọc thông tin từ socket
+            sentence_from_client = inFromClient.readLine();
+            
+            sentence_to_client = sentence_from_client +" (Server accepted!)" + '\n';
+            //ghi dữ liệu ra socket
+            outToClient.writeBytes(sentence_to_client); 
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+            //return này dùng để ngắt vòng while, ngưng chạy server
+            //return;
+        }
+        
     }
 }
